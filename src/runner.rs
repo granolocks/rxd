0e866e39a75e859ee3be4c709b186194871f67a6
Gabe Koss <gabe.koss@gmail.com>
2022-10-23 19:25:11 -0400

    some tests

--- src/runner.rs
+++ src/runner.rs
@@ -1,5 +1,5 @@
-use crate::reader::Reader;
 use crate::byte::Byte;
+use crate::reader::Reader;
 
 const LINE_LENGTH: usize = 16;
 
@@ -13,9 +13,7 @@ impl Runner {
         let mut reader = Reader::new(file_path);
         let bytes = reader.read().unwrap();
 
-        Self {
-            bytes,
-        }
+        Self { bytes }
     }
 
     pub fn print_lines(&self) {
@@ -36,7 +34,7 @@ impl Runner {
     }
 }
 
-fn bytes_to_hex_string(bytes: &Vec<Byte>) -> String {
+pub fn bytes_to_hex_string(bytes: &Vec<Byte>) -> String {
     bytes
         .iter()
         .map(|b| b.to_hex_string())
@@ -47,10 +45,43 @@ fn bytes_to_hex_string(bytes: &Vec<Byte>) -> String {
         .join(" ")
 }
 
-fn bytes_to_ascii_string(bytes: &Vec<Byte>) -> String {
+pub fn bytes_to_ascii_string(bytes: &Vec<Byte>) -> String {
     bytes
         .iter()
         .map(|b| b.to_ascii())
         .collect::<Vec<String>>()
         .join("")
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    #[test]
+    fn test_bytes_to_ascii_string() {
+        let ascii_range = (0u8..255u8)
+            .map(|b| Byte::new(b).to_ascii())
+            .collect::<Vec<String>>()
+            .join((""));
+        assert_eq!(ascii_range, "................................ !\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~..................................¡¢£¤¥¦§¨©ª«¬\u{ad}®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþ")
+    }
+
+    #[test]
+    fn test_bytes_to_hex_string() {
+        let v_empty: Vec<Byte> = vec![];
+        assert_eq!(String::from(""), bytes_to_hex_string(&v_empty));
+
+        let v0 = vec![Byte::new(0)];
+        assert_eq!(String::from("00"), bytes_to_hex_string(&v0));
+
+        let v1 = vec![Byte::new(0), Byte::new(255)];
+        assert_eq!(String::from("00ff"), bytes_to_hex_string(&v1));
+
+        let v2 = vec![
+            Byte::new(0xde),
+            Byte::new(0xad),
+            Byte::new(0xbe),
+            Byte::new(0xef),
+        ];
+        assert_eq!(String::from("dead beef"), bytes_to_hex_string(&v2));
+    }
 }
